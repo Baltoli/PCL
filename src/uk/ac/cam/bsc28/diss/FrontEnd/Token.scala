eb9d96ccfdc8243043a06e9fe9e9babea9288c8b
Bruce Collie <bsc28@cam.ac.uk>
2015-11-29 12:00:16 +0000

    Begin to add tokens.

---
+++ src/uk/ac/cam/bsc28/diss/FrontEnd/Token.scala
@@ -0,0 +1,23 @@
+package uk.ac.cam.bsc28.diss.FrontEnd
+
+abstract class Token
+
+case class VarName(s: String) extends Token
+case class Type(t: String) extends Token
+case class IntegerLiteral(i: Int) extends Token
+case class Operator(o: String) extends Token
+case class In() extends Token
+case class Out() extends Token
+case class New() extends Token
+case class Parallel() extends Token
+case class Sequential() extends Token
+case class End() extends Token
+case class Replicate() extends Token
+case class OpenBracket() extends Token
+case class CloseBracket() extends Token
+case class OpenSquareBracket() extends Token
+case class CloseSquareBracket() extends Token
+case class OpenCurlyBracket() extends Token
+case class CloseCurlyBracket() extends Token
+case class Equals() extends Token
+case class TypedAs() extends Token
