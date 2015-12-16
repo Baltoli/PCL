99f668064a28a7233c927074c9d2ca2e989f7340
Bruce Collie <bsc28@cam.ac.uk>
2015-12-16 17:11:08 +0000

    Write about receive nondeterminism.

--- src/uk/ac/cam/bsc28/diss/VM/Scheduler.scala
+++ src/uk/ac/cam/bsc28/diss/VM/Scheduler.scala
@@ -19,6 +19,15 @@ object Scheduler {
     }
   }
 
+  // This notification method performs a unicast-like operation
+  // to send an atom to an interpreter. Nondeterminism is handled
+  // by sending to the first registered interpreter that is blocked
+  // on the channel c.
+  // Possible extensions here include implementing a multicast
+  // version that sends to all available channels instead of just one
+  // (as the pi-calculus reduction semantics are unicast). Need to write
+  // about the nondeterminism strategy used in the report (i.e. how
+  // the choice of strategy can affect execution of programs).
   def notifyAll(c: Channel, v: Atom): Unit = {
     var sent = false
     all foreach { i =>
