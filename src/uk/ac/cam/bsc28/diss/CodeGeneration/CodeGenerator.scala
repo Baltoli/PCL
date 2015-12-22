14b01f725ee475be2fd417e10d41fdab41b2864f
Bruce Collie <bsc28@cam.ac.uk>
2015-12-22 13:14:14 +0000

    Add empty code generator skeleton.

---
+++ src/uk/ac/cam/bsc28/diss/CodeGeneration/CodeGenerator.scala
@@ -0,0 +1,12 @@
+package uk.ac.cam.bsc28.diss.CodeGeneration
+
+import uk.ac.cam.bsc28.diss.FrontEnd.ParseTree.Program
+import uk.ac.cam.bsc28.diss.VM.Instruction
+
+class CodeGenerator(prog: Program) {
+
+  def generate(): List[Instruction] = {
+    List()
+  }
+
+}
